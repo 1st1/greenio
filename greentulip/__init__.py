5914cad75d6530ec34f0bfb6746b7792a0bb4a34
Yury Selivanov <yselivanov@sprymix.com>
2013-06-08 15:33:35 -0400

    greenlet: Document "yield_from" and "GreenTask._step"

--- greentulip/__init__.py
+++ greentulip/__init__.py
@@ -11,6 +11,7 @@ __all__ = 'task', 'yield_from'
 
 
 import greenlet
+
 import tulip
 from tulip import unix_events, tasks, futures
 
@@ -34,18 +35,34 @@ class GreenTask(tulip.Task):
 
     def _step(self, value=tasks._marker, exc=None):
         if self._greenlet is None:
+            # Means that the task is not currently in a suspended greenlet
+            # waiting for results for "yield_from"
             ovr = super()._step
             self._greenlet = _TaskGreenlet(ovr)
+
+            # Store a reference to the current task for "yield_from"
             self._greenlet.task = self
+
+            # Now invoke overloaded "Task._step" in "_TaskGreenlet"
             result = self._greenlet.switch(value, exc)
+
+            # If "result" is "_YIELDED" it means that the "yield_from"
+            # method was called
             if result is not _YIELDED:
+                # And if not - then task jumped out of greenlet without
+                # calling "yield_from"
                 self._greenlet.task = None
                 self._greenlet = None
         else:
+            # The task is in the greenlet, that means that we have a result
+            # for the "yield_from"
             if exc is not None:
                 result = self._greenlet.throw(type(exc), exc, exc.__traceback__)
             else:
                 result = self._greenlet.switch(value)
+
+            # Again, if "result" is "_YIELDED" then we just called "yield_from"
+            # again
             if result is not _YIELDED:
                 self._greenlet.task = None
                 self._greenlet = None
@@ -87,11 +104,13 @@ def yield_from(future):
             raise RuntimeError(
                     '"greentulip.yield_from" requires GreenEventLoopPolicy '
                     'or compatible')
+            # or something went horribly wrong...
 
         if not isinstance(gl, _TaskGreenlet):
             raise RuntimeError(
                     '"greentulip.yield_from" was supposed to be called from a '
                     '"greentulip.task" or a subsequent coroutine')
+            # ...ditto
 
     task = gl.task
 
@@ -100,6 +119,8 @@ def yield_from(future):
                 'greenlet.yield_from was supposed to receive only Futures, '
                 'got {!r} in task {!r}'.format(future, task))
 
+    # "_wakeup" will call the "_step" method (which we overloaded in
+    # GreenTask, and therefore wakeup the awaiting greenlet)
     future.add_done_callback(task._wakeup)
     task._fut_waiter = future
 
@@ -107,6 +128,7 @@ def yield_from(future):
     if task._must_cancel:
         talk._fut_waiter.cancel()
 
+    # Jump out of the current task greenlet (we'll return to GreenTask._step)
     return gl.parent.switch(_YIELDED)
 
 
