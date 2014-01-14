e9333a807d718a77cdbf102e16bf52c49d0711a6
Yury Selivanov <yselivanov@sprymix.com>
2014-01-14 11:40:40 -0500

    Make setup.py ready for release on PyPI

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
-from distutils.core import setup
+from setuptools import setup
+
+
+extra = {}
+f = open('README', 'r')
+try:
+    extra['long_description'] = f.read()
+finally:
+    f.close()
 
 
 setup(
     name='greenio',
-    description="Greenlet based implementation of PEP 3156 event loop.",
+    version='0.5.0',
+    description="Greenlets for asyncio (PEP 3156).",
     url='https://github.com/1st1/greenio/',
     license='Apache 2.0',
     packages=['greenio'],
